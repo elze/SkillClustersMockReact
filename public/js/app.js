35b5b14e2e3b7806fc5972f4f9a7c8a3b25c63f0
Elze Hamilton <elzes@yahoo.com>
2016-12-04 14:09:30 -0600

    Getting the list of primary skills from the backend

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,20 @@
 class PrimarySkills extends React.Component {
 constructor(props) {
     super(props);
-    this.state = {primarySkills: [{title: "C#"}, {title: ".NET"}, {title: "Javascript"}, {title: "Angular.js"}]};
+    this.state = {primarySkills: [{primary_term: "C#"}, {primary_term: ".NET"}, {primary_term: "Javascript"}, {primary_term: "Angular.js"}, {primary_term: "React.js"}]};
   }
 
   componentDidMount() {
+    //this.state = {primarySkills: [{title: "C#"}, {title: ".NET"}, {title: "Javascript"}, {title: "Angular.js"}, {title: "React.js"}, {title: "Backbone.js"}]};
 
+    //var self = this;
+    fetch(`http://localhost:5000/skills`)
+      .then(response => response.json())
+      .then(json =>
+	    this.setState({
+	      primarySkills: json
+		  })
+	    )
   }
 
   componentWillUnmount() {
@@ -17,7 +26,7 @@ constructor(props) {
     return (
       <ul>
       {this.state.primarySkills.map((primarySkill, i) =>
-          <li class="primary" key={i}>{primarySkill.title}</li>
+          <li class="primary" key={i}>{primarySkill.primary_term}</li>
 			    )}
       </ul>
     );
